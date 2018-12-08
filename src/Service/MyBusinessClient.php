6ffe49e3ee0ecab510bb597b701ba85f931f9d0a
dakku <dakku@97634.no-reply.drupal.org>
2018-12-08 16:02:46 +0000

    Refactor to use with  google_api_client module

--- src/Service/MyBusinessClient.php
+++ src/Service/MyBusinessClient.php
@@ -5,8 +5,9 @@ namespace Drupal\google_mybusiness_api\Service;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\google_api_client\Service\GoogleApiClient;
+use Exception;
 use Google_Service_MyBusiness;
-
 
 /**
  * Class Google My Business API Client Service.
@@ -30,16 +31,31 @@ class MyBusinessClient {
   private $config;
 
   /**
+   * Uneditable Tokens Config.
+   *
+   * @var \Drupal\Core\Config\Config|\Drupal\Core\Config\ImmutableConfig
+   */
+  private $configTokens;
+
+  /**
    * Cache.
    * @var \Drupal\Core\Cache\CacheBackendInterface
    */
   private $cacheBackend;
 
   /**
-   * Google Service MyBusiness
+   * Google MyBusiness Service
+   *
    * @var \Google_Service_MyBusiness
    */
-  private $googleMyBusiness;
+  public $googleServiceMyBusiness;
+
+  /**
+   * Google API Client.
+   *
+   * @var \Drupal\google_api_client\Service\GoogleApiClient
+   */
+  private $googleApiClient;
 
   /**
    * Callback Controller constructor.
@@ -50,14 +66,38 @@ class MyBusinessClient {
    *   LoggerChannelFactoryInterface.
    * @param \Drupal\Core\Cache\CacheBackendInterface $cacheBackend
    *   Cache Backend.
+   * @param \Drupal\google_api_client\Service\GoogleApiClient $googleApiClient
    */
   public function __construct(ConfigFactory $config,
                               LoggerChannelFactoryInterface $loggerFactory,
-                              CacheBackendInterface $cacheBackend) {
-    $this->config = $config->get('google_mybusiness_api.settings');
+                              CacheBackendInterface $cacheBackend,
+                              GoogleApiClient $googleApiClient) {
+    $this->config = $config->get('google_api_client.settings');
+    $this->configTokens = $config->get('google_api_client.tokens');
+
     $this->loggerFactory = $loggerFactory;
     $this->cacheBackend = $cacheBackend;
-    $this->googleMyBusiness = new Google_Service_MyBusiness();
+
+    $this->googleApiClient = $googleApiClient;
+    $this->googleServiceMyBusiness = $this->getGoogleMyBusinessClient();
+  }
+
+  private function getGoogleMyBusinessClient() {
+    // See https://developers.google.com/my-business/content/basic-setup
+    $this->googleApiClient->googleClient->setScopes(["https://www.googleapis.com/auth/plus.business.manage"]);
+    $googleServiceMyBusiness = new Google_Service_MyBusiness($this->googleApiClient->googleClient);
+    return $googleServiceMyBusiness;
+  }
+
+  public function doRequest() {
+    try {
+      $accounts = $this->googleServiceMyBusiness->accounts->listAccounts();
+      //ksm($accounts);
+    }
+    catch (Exception $e) {
+      //ksm($e);
+    }
+
   }
 
 
